4195c18362d8267ba5d74e127a851767ca15f963
CodingWithAndres <146770534+CodingWithAndres@users.noreply.github.com>
2023-10-04 10:03:54 -0500

    WarmUp

---
+++ WarmUpOct4th/src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        // Scenario 1: Storing a person's name
+// Create a String variable named "personName" and set it to "John Doe"
+        String personName = "John Doe";
+
+// Scenario 2: Storing a person's age
+// Create an int variable named "personAge" and set it to 25
+        int personAge = 25;
+
+// Scenario 3: Storing the price of a product
+// Create a double variable named "productPrice" and set it to 19.99
+        double productPrice = 19.99;
+
+// Scenario 4: Storing the temperature in Celsius
+// Create a float variable named "temperatureCelsius" and set it to 27.5
+        float temperatureCelsius = 27.5f;
+
+// Scenario 5: Storing a login status
+// Create a boolean variable named "isLoggedIn" and set it to true
+        boolean isLoggedIn = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
