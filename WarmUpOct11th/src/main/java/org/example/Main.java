3b64f3a6c1a4b644c5929902aee39ee77279eccc
CodingWithAndres <146770534+CodingWithAndres@users.noreply.github.com>
2023-10-11 09:15:14 -0500

    Fri Oct 11th

---
+++ WarmUpOct11th/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //even numbers using a for loop
+        System.out.println("Even numbers from 1 to 20:");
+        for (int i = 2; i <= 20; i += 2) {
+            System.out.print(i + " even number ");
+        }
+
+        //odd numbers using a while loop
+        System.out.println("Odd numbers from 1 to 19:");
+        int j = 1;
+        while (j <= 19) {
+            System.out.print(j + " odd number ");
+            j += 2;
+        }
+            }
+        }
